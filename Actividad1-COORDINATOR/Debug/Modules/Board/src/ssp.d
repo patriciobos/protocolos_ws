e80652e2fcb50475a900407f2ab1f0a1d8c3ecd6
patriciobos <patriciobos@gmail.com>
2016-06-06 00:15:02 -0300

    creo un workspace de LPCXpresso dentro del repo

--- Actividad1-COORDINATOR/Debug/Modules/Board/src/ssp.d
+++ Actividad1-COORDINATOR/Debug/Modules/Board/src/ssp.d
@@ -1,35 +1,35 @@
 Modules/Board/src/ssp.o Modules/Board/src/ssp.d: \
  ../Modules/Board/src/ssp.c \
- /home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/Actividad1-COORDINATOR/src/includes.h \
- /home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/CMSISv2p00_LPC13xx/inc/LPC13xx.h \
- /home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/CMSISv2p00_LPC13xx/inc/core_cm3.h \
- /home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/CMSISv2p00_LPC13xx/inc/core_cmInstr.h \
- /home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/CMSISv2p00_LPC13xx/inc/core_cmFunc.h \
- /home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/CMSISv2p00_LPC13xx/inc/system_LPC13xx.h \
- /home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/Actividad1-COORDINATOR/Modules/Board/inc/ssp.h \
- /home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/Actividad1-COORDINATOR/Modules/cc2520/inc/cc2520.h \
- /home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/Actividad1-COORDINATOR/Modules/cc2520/inc/cc2520-mac.h \
- /home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/Actividad1-COORDINATOR/Modules/cc2520/inc/cc2520-task.h \
- /home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/Actividad1-COORDINATOR/Modules/Board/inc/ledpul.h
+ /home/patricio/Embebidos/Protocolos/protocolos_ws/Actividad1-COORDINATOR/src/includes.h \
+ /home/patricio/Embebidos/Protocolos/protocolos_ws/CMSISv2p00_LPC13xx/inc/LPC13xx.h \
+ /home/patricio/Embebidos/Protocolos/protocolos_ws/CMSISv2p00_LPC13xx/inc/core_cm3.h \
+ /home/patricio/Embebidos/Protocolos/protocolos_ws/CMSISv2p00_LPC13xx/inc/core_cmInstr.h \
+ /home/patricio/Embebidos/Protocolos/protocolos_ws/CMSISv2p00_LPC13xx/inc/core_cmFunc.h \
+ /home/patricio/Embebidos/Protocolos/protocolos_ws/CMSISv2p00_LPC13xx/inc/system_LPC13xx.h \
+ /home/patricio/Embebidos/Protocolos/protocolos_ws/Actividad1-COORDINATOR/Modules/Board/inc/ssp.h \
+ /home/patricio/Embebidos/Protocolos/protocolos_ws/Actividad1-COORDINATOR/Modules/cc2520/inc/cc2520.h \
+ /home/patricio/Embebidos/Protocolos/protocolos_ws/Actividad1-COORDINATOR/Modules/cc2520/inc/cc2520-mac.h \
+ /home/patricio/Embebidos/Protocolos/protocolos_ws/Actividad1-COORDINATOR/Modules/cc2520/inc/cc2520-task.h \
+ /home/patricio/Embebidos/Protocolos/protocolos_ws/Actividad1-COORDINATOR/Modules/Board/inc/ledpul.h
 
-/home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/Actividad1-COORDINATOR/src/includes.h:
+/home/patricio/Embebidos/Protocolos/protocolos_ws/Actividad1-COORDINATOR/src/includes.h:
 
-/home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/CMSISv2p00_LPC13xx/inc/LPC13xx.h:
+/home/patricio/Embebidos/Protocolos/protocolos_ws/CMSISv2p00_LPC13xx/inc/LPC13xx.h:
 
-/home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/CMSISv2p00_LPC13xx/inc/core_cm3.h:
+/home/patricio/Embebidos/Protocolos/protocolos_ws/CMSISv2p00_LPC13xx/inc/core_cm3.h:
 
-/home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/CMSISv2p00_LPC13xx/inc/core_cmInstr.h:
+/home/patricio/Embebidos/Protocolos/protocolos_ws/CMSISv2p00_LPC13xx/inc/core_cmInstr.h:
 
-/home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/CMSISv2p00_LPC13xx/inc/core_cmFunc.h:
+/home/patricio/Embebidos/Protocolos/protocolos_ws/CMSISv2p00_LPC13xx/inc/core_cmFunc.h:
 
-/home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/CMSISv2p00_LPC13xx/inc/system_LPC13xx.h:
+/home/patricio/Embebidos/Protocolos/protocolos_ws/CMSISv2p00_LPC13xx/inc/system_LPC13xx.h:
 
-/home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/Actividad1-COORDINATOR/Modules/Board/inc/ssp.h:
+/home/patricio/Embebidos/Protocolos/protocolos_ws/Actividad1-COORDINATOR/Modules/Board/inc/ssp.h:
 
-/home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/Actividad1-COORDINATOR/Modules/cc2520/inc/cc2520.h:
+/home/patricio/Embebidos/Protocolos/protocolos_ws/Actividad1-COORDINATOR/Modules/cc2520/inc/cc2520.h:
 
-/home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/Actividad1-COORDINATOR/Modules/cc2520/inc/cc2520-mac.h:
+/home/patricio/Embebidos/Protocolos/protocolos_ws/Actividad1-COORDINATOR/Modules/cc2520/inc/cc2520-mac.h:
 
-/home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/Actividad1-COORDINATOR/Modules/cc2520/inc/cc2520-task.h:
+/home/patricio/Embebidos/Protocolos/protocolos_ws/Actividad1-COORDINATOR/Modules/cc2520/inc/cc2520-task.h:
 
-/home/patricio/Embebidos/Protocolos/Practica/ws-1343\ hola\ mundo/Actividad1-COORDINATOR/Modules/Board/inc/ledpul.h:
+/home/patricio/Embebidos/Protocolos/protocolos_ws/Actividad1-COORDINATOR/Modules/Board/inc/ledpul.h:
